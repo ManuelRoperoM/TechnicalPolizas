9b3598077121f01e0f3c069aa1b6d675aab3273c
ManuelR <mnlrpr@gmail.com>
2026-07-04 07:38:33 -0500

    Creacion de los repository

---
+++ src/main/java/com/test/technical_polizas/poliza/repository/PolizaRepository.java
@@ -0,0 +1,7 @@
+package com.test.technical_polizas.poliza.repository;
+
+import com.test.technical_polizas.poliza.entity.Poliza;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PolizaRepository extends JpaRepository <Poliza, Long> {
+}
---
+++ src/main/java/com/test/technical_polizas/riesgo/repository/RiesgoRepository.java
@@ -0,0 +1,7 @@
+package com.test.technical_polizas.riesgo.repository;
+
+import com.test.technical_polizas.riesgo.entity.Riesgo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RiesgoRepository extends JpaRepository<Riesgo, Long> {
+}
